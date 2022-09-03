60745c277e04e004a3afbadc381c50846dd43956
PedroMoraesMilagres <pedromoraesmilagres@gmail.com>
2022-09-03 19:26:13 -0300

    added SDLKeymap.hpp

---
+++ include/SDLKeymap.hpp
@@ -0,0 +1,9 @@
+#include <SDL2/SDL.h>
+
+const static unsigned char SDLKeymap[16] = 
+{
+    SDLK_x, SDLK_1, SDLK_2, SDLK_3,    // 0 1 2 3
+    SDLK_q, SDLK_w, SDLK_e, SDLK_a,   // 4 5 6 7
+    SDLK_s, SDLK_d, SDLK_z, SDLK_c,   // 8 9 A B
+    SDLK_4, SDLK_r, SDLK_f, SDLK_v    // C D E F
+};
