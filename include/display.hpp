638b034e551945885071bc6301d7ee43e34d0f45
PedroMoraesMilagres <pedromoraesmilagres@gmail.com>
2022-09-04 18:24:25 -0300

    added display Class.

---
+++ include/display.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdlib>
+#include <SDL2/SDL.h>
+
+
+
+class Display
+{
+    public:
+        Display();
+        ~Display();
+        void KeysInput();
+    private:
+    	  SDL_Window* window{};
+	      SDL_Renderer* renderer{};
+	      SDL_Texture* texture{};
+};
+
